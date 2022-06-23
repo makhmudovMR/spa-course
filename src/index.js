aaef1868184e152ff0a64f68a07567ff7460436f
magomed <mmriadovich@gmail.com>
2022-06-23 22:47:00 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import 'materialize-css/dist/css/materialize.min.css'
+
+console.log('App is working')
+document.querySelector('h1').textContent = 'Working'
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  }
+};
