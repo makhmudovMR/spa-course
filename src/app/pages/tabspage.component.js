87faa59d054ecd4e1e65e7a21fe6fced79cc7da4
magomed <mmriadovich@gmail.com>
2022-06-30 00:44:51 +0300

    lesson 5

--- src/app/pages/tabspage.component.js
+++ src/app/pages/tabspage.component.js
@@ -1,14 +1,59 @@
 import { CoreComponent } from '../../framework'
 
 class TabsPageComponent extends CoreComponent {
+
   constructor(config) {
     super(config)
+  }
+
+  events() {
+    return {
+      'click .collapsible': 'onTabClick'
+    }
+  }
+
+  onTabClick({target}) {
+    if (!target.classList.contains('collapsible-header')) return
+    this.el.querySelectorAll('.js-tab').forEach(e => e.classList.remove('active'))
+
+    target.parentNode.classList.add('active')
   }
 }
 
 const tabsPageComponent = new TabsPageComponent({
   selector: 'app-home-page',
-  template: '<h1>Tabs page</h1>'
+  template: `
+    <ul class="collapsible collapsible-accordion" style="width: 50%; margin: 10px auto">
+      <li class="js-tab">
+        <div class="collapsible-header" tabindex="0">First</div>
+        <div class="collapsible-body" style="">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+        </div>
+      </li>
+      <li class="js-tab">
+        <div class="collapsible-header" tabindex="0">Second</div>
+        <div class="collapsible-body" style="">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.</span>
+        </div>
+      </li>
+      <li class="js-tab">
+        <div class="collapsible-header" tabindex="0">Third</div>
+        <div class="collapsible-body" style="">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.</span>
+        </div>
+      </li>
+    </ul>
+  `
 })
 
 export { tabsPageComponent }
--- src/framework/core/component.js
+++ src/framework/core/component.js
@@ -1,4 +1,6 @@
+
 export class Component {
+
   constructor(config) {
     this.template = config.template
     this.selector = config.selector
@@ -10,6 +12,16 @@ export class Component {
     if(!this.el) {
       throw new Error(`Component with ${this.selector} selector not found`);
     }
-    document.querySelector(this.selector).innerHTML = this.template
+    this.el.innerHTML = this.template
+    this._initEvents()
+  }
+
+  _initEvents() {
+    if(this.events === undefined) return
+    const events = this.events()
+    Object.keys(events).forEach((key) => {
+      let listener = key.split(' ')
+      this.el.querySelector(listener[1]).addEventListener(listener[0], this[events[key]].bind(this))
+    })
   }
 }
